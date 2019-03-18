ba85ffd3b2eaa86a45b6ac158e5f8083bab10f27
王强 <actstrady@tom.com>
2019-03-18 09:08:02 +0800

    精度损失TODO

--- src/main/java/Basic.java
+++ src/main/java/Basic.java
@@ -1,5 +1,8 @@
+import com.sun.xml.internal.bind.v2.TODO;
+
 public class Basic {
     public static void main(String[] args) {
+        // TODO 精度损失 2019.3.18
         double a = 16777217;
         System.out.println(a);
     }
